e5b0c6550e3646724fae6e4747f8c292d6cc17ba
Freek Bes <freekbladnl@gmail.com>
2021-09-20 17:03:17 +0200

    bugfix

--- sort_five.c
+++ sort_five.c
@@ -6,7 +6,7 @@
 /*   By: fbes <fbes@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/20 12:51:24 by fbes          #+#    #+#                 */
-/*   Updated: 2021/09/20 13:34:53 by fbes          ########   odam.nl         */
+/*   Updated: 2021/09/20 17:02:38 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,23 +21,38 @@ void	sort_five(t_stack *a, t_stack *b)
 		pb(a, b);
 	while (!is_sorted(a, 0))
 		sort_three(a);
+	while (!is_sorted(b, 0))
+		sort_three(b);
 	print_stack(a);
+	i = 0;
 	while (b->length > 0)
 	{
-		i = 0;
-		while (a->stack[a->length - 1] < b->stack[b->length - 1])
+		if (a->stack[0] < b->stack[b->length - 1])
 		{
-			ra(a);
-			i++;
+			while (i < a->length)
+			{
+				ra(a);
+				print_stack(a);
+				i++;
+			}
+		}
+		else
+		{
+			while (a->stack[a->length - 1] < b->stack[b->length - 1])
+			{
+				ra(a);
+				print_stack(a);
+				i++;
+			}
 		}
 		pa(a, b);
 		print_stack(a);
-		while (i > 0)
-		{
-			rra(a);
-			i--;
-		}
 	}
-	print_stack(a);
+	while (i > 0)
+	{
+		rra(a);
+		print_stack(a);
+		i--;
+	}
 	exit(0);
 }

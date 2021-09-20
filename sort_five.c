ecbe2e3882eef5aa99079d5a290b80a64b74400f
Freek Bes <freekbladnl@gmail.com>
2021-09-20 13:35:26 +0200

    fixed sorting of sort_five, still need less steps

--- sort_five.c
+++ sort_five.c
@@ -6,7 +6,7 @@
 /*   By: fbes <fbes@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/20 12:51:24 by fbes          #+#    #+#                 */
-/*   Updated: 2021/09/20 13:21:51 by fbes          ########   odam.nl         */
+/*   Updated: 2021/09/20 13:34:53 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,19 @@ void	sort_five(t_stack *a, t_stack *b)
 	print_stack(a);
 	while (b->length > 0)
 	{
-		pa(a, b);
-		// still need to figure out how to put the number from b
-		// at the right spot in a
-		print_stack(a);
-		if (a->stack[a->length - 1] > a->stack[a->length - 2])
+		i = 0;
+		while (a->stack[a->length - 1] < b->stack[b->length - 1])
+		{
 			ra(a);
+			i++;
+		}
+		pa(a, b);
+		print_stack(a);
+		while (i > 0)
+		{
+			rra(a);
+			i--;
+		}
 	}
 	print_stack(a);
 	exit(0);

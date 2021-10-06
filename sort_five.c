061795275c8882c2dbbcf9d0fa2105e238346cea
Freek Bes <freekbladnl@gmail.com>
2021-10-06 22:33:45 +0200

    bugfix

--- sort_five.c
+++ sort_five.c
@@ -6,7 +6,7 @@
 /*   By: fbes <fbes@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/20 12:51:24 by fbes          #+#    #+#                 */
-/*   Updated: 2021/10/06 22:27:33 by fbes          ########   odam.nl         */
+/*   Updated: 2021/10/06 22:32:50 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,48 +50,38 @@ void	sort_five(t_stack *a, t_stack *b)
 				frame = frame->prev;
 				steps_back++;
 			}
+			ft_putstr_fd("steps_front: ", 1);
+			ft_putnbr_fd(steps_front, 1);
+			ft_putstr_fd("\nsteps_back: ", 1);
+			ft_putnbr_fd(steps_back, 1);
+			ft_putchar_fd('\n', 1);
+			i = 0;
+			while (steps_front <= steps_back && i < steps_front)
+			{
+				ra(a);
+				i++;
+				steps_taken++;
+			}
+			while (steps_front > steps_back && i < steps_back)
+			{
+				rra(a);
+				i++;
+				steps_taken--;
+			}
+			print_stack(a);
+			print_stack(b);
+			pa(a, b);
+			print_stack(a);
+			print_stack(b);
+			ft_putstr_fd("steps_taken: ", 1);
+			ft_putnbr_fd(steps_taken, 1);
+			ft_putchar_fd('\n', 1);
 		}
 		else
 		{
-			ft_putendl_fd("going to find biggest pos", 1);
-			while (steps_front < a->size && frame != biggest)
-			{
-				steps_front++;
-				frame = frame->next;
-			}
-			frame = get_stack_bottom(a);
-			while (steps_back < a->size && frame != biggest)
-			{
-				steps_back++;
-				frame = frame->prev;
-			}
-		}
-		ft_putstr_fd("steps_front: ", 1);
-		ft_putnbr_fd(steps_front, 1);
-		ft_putstr_fd("\nsteps_back: ", 1);
-		ft_putnbr_fd(steps_back, 1);
-		ft_putchar_fd('\n', 1);
-		i = 0;
-		while (steps_front <= steps_back && i < steps_front)
-		{
+			pa(a, b);
 			ra(a);
-			i++;
-			steps_taken++;
 		}
-		while (steps_front > steps_back && i < steps_back)
-		{
-			rra(a);
-			i++;
-			steps_taken--;
-		}
-		print_stack(a);
-		print_stack(b);
-		pa(a, b);
-		print_stack(a);
-		print_stack(b);
-		ft_putstr_fd("steps_taken: ", 1);
-		ft_putnbr_fd(steps_taken, 1);
-		ft_putchar_fd('\n', 1);
 	}
 	while (steps_taken > 0)
 	{
